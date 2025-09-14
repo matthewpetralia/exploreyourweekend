d82c705a7d34532bece3b4c8d0415d59218fb00c
Matthew Petralia <petraliamatthew@gmail.com>
2025-09-14 13:32:24 +1000

    Began transition to icon.liquid for svgs Changed Google Maps to contain a heading and paragraph

--- search-logic.js
+++ search-logic.js
@@ -198,7 +198,7 @@ function renderActiveTags(context) {
         // Use the helper to format for display
         tagElement.textContent = tag;
         const removeButton = document.createElement("button");
-        removeButton.innerHTML = "<span class='material-symbols-outlined'>close</span>";
+        removeButton.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 -960 960 960' ><path d='m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z'/></svg>";
         removeButton.classList.add("remove-tag-button");
         removeButton.addEventListener("click", () => {
             toggleTag(tag);
