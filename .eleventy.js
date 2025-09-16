4094018f922bd3816a3abbd0add5fb1172fc59d9
Matthew Petralia <petraliamatthew@gmail.com>
2025-09-16 22:41:28 +1000

    testing more image ops

--- .eleventy.js
+++ .eleventy.js
@@ -3,39 +3,44 @@ const util = require('util');
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addFilter("dump", obj => {
-        return JSON.stringify(obj, null, 2);
-    });
+    eleventyConfig.addFilter("dump", obj => {
+        return JSON.stringify(obj, null, 2);
+    });
 
-    eleventyConfig.addFilter("safe", obj => {
-        return obj;
-    });
+    eleventyConfig.addFilter("safe", obj => {
+        return obj;
+    });
 
-    eleventyConfig.addPassthroughCopy("main.css");
-    eleventyConfig.addPassthroughCopy("code.js");
-    eleventyConfig.addPassthroughCopy("robots.txt");
-    eleventyConfig.addPassthroughCopy("_redirects");
-    eleventyConfig.addPassthroughCopy("search-logic.js");
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy(".eslintrc.js");
-    eleventyConfig.addPassthroughCopy("CNAME");
-    eleventyConfig.addPassthroughCopy("ExploreYourWeekendFAVICON.svg");
-    eleventyConfig.addPassthroughCopy("ExploreYourWeekendLogo.svg");
-    eleventyConfig.addPassthroughCopy("ExploreYourWeekendLogoGREEN.svg");
-    eleventyConfig.addPassthroughCopy("normalize.css");
-    eleventyConfig.addPassthroughCopy({"./node_modules/lunr/lunr.js": "assets/js/lunr.js"});
+    eleventyConfig.addPassthroughCopy("main.css");
+    eleventyConfig.addPassthroughCopy("code.js");
+    eleventyConfig.addPassthroughCopy("robots.txt");
+    eleventyConfig.addPassthroughCopy("_redirects");
+    eleventyConfig.addPassthroughCopy("search-logic.js");
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy(".eslintrc.js");
+    eleventyConfig.addPassthroughCopy("CNAME");
+    eleventyConfig.addPassthroughCopy("ExploreYourWeekendFAVICON.svg");
+    eleventyConfig.addPassthroughCopy("ExploreYourWeekendLogo.svg");
+    eleventyConfig.addPassthroughCopy("ExploreYourWeekendLogoGREEN.svg");
+    eleventyConfig.addPassthroughCopy("normalize.css");
+    eleventyConfig.addPassthroughCopy({"./node_modules/lunr/lunr.js": "assets/js/lunr.js"});
 
-    eleventyConfig.addCollection("guides", async function(collectionApi) {
-        // The correct way to handle async data in Eleventy collections
-        const guidesData = await require("./_data/guides.js")();
-        return guidesData;
-    });
-
-    return {
-        dir: {
-            input: ".",
-            output: "_site",
-            data: "_data"
-        }
-    };
+    eleventyConfig.addCollection("guides", async function(collectionApi) {
+        const guidesData = await require("./_data/guides.js")();
+        return guidesData;
+    });
+   
+    // ADDED: New collection for all locations
+    eleventyConfig.addCollection("locations", async function(collectionApi) {
+        const locationsData = await require("./_data/locations.js")();
+        return locationsData;
+    });
+   
+    return {
+        dir: {
+            input: ".",
+            output: "_site",
+            data: "_data"
+        }
+    };
 };
